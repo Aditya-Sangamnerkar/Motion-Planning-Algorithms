9e838f9a5144e370d2a6d3da7318ce873e8f33ee
Aditya Sangamnerkar <sangamnerkaram@rknec.edu>
2021-05-03 15:45:29 +0530

    real rrt code complete

--- rrt.py
+++ rrt.py
@@ -1,12 +1,8 @@
-
-'''
- - The input for obstacles can be given by modifying the 'draw the obstacles portion of the code'
- - The initial and destination points should be provided via mouse clicks when the program is executed
-'''
 import pygame
 import random
 import sys
 import copy
+
 
 class Point():
 
@@ -22,28 +18,30 @@ class Point():
 			if(equation_value <= (obstacle.radius ** 2)):
 				return True
 		return False
-	def isCrossingObstacle(self,point2):
-		# the obstacles are circles in shape so the intersection of a line and circle calculations were done to arrive at the formulae used in this function.
+
+	def isCrossingObstacle(self,point2,segments):
 		p = point2.coordinate[1]-self.coordinate[1]
 		q = point2.coordinate[0]-self.coordinate[0]
-		# for the simplicity of implementation the point that will form line parallel to y axis is ignored
-		if(q == 0):
+		x1 = self.coordinate[0]
+		y1 = self.coordinate[1]
+		x2 = point2.coordinate[0]
+		y2 = point2.coordinate[1]
+		m = 0
+		c = 0
+		if q == 0:
 			return True
-		m = p/q
-		x = self.coordinate[0]
-		y = self.coordinate[1]
-		c = y - m*x
-		for obstacle in obstacles:
-			h = obstacle.h
-			k = obstacle.k
-			r = obstacle.radius
-			a = 1 + (m*m)
-			b = (2*m*c) - (2*m*k) - (2*h)
-			c = (h**2) + (c**2) + (k**2) - (r**2) - (2*c*k) 
-			discriminant = (b**2) - (4*a*c)
-			if(discriminant>=0):
+		else:
+			m = p/q
+			c = y1 - (m*x1)
+		for x in range(int(x1),int(x2),segments):			
+			y = m*x + c
+			qNode = Point()
+			qNode.coordinate = (x,y)
+			if(qNode.isInObstacle()):
 				return True
 		return False
+			
+
 	def calculateDistance(self,point2):
 		# eucledian distance formula is used to calculate the distance between two points.
 		x = (self.coordinate[0] - point2.coordinate[0])**2
@@ -51,6 +49,17 @@ class Point():
 		distance = (x + y) ** (0.5)
 		return distance
 
+	def generateNewNode(self,point2,epsilon):
+		x1 = self.coordinate[0]
+		y1 = self.coordinate[1]
+		x2 = point2.coordinate[0]
+		y2 = point2.coordinate[1]
+		qNew = Point()
+		r = self.calculateDistance(point2)
+		x = x1 + epsilon*((x2-x1)/r)
+		y = y1 + epsilon*((y2-y1)/r)
+		qNew.coordinate = (x,y)
+		return qNew
 
 class Obstacle():
 
@@ -63,7 +72,9 @@ class Obstacle():
 		obstacles.append(self)
 
 # RRT Parameters
+epsilon = 20
 max_connect_length = 50
+segments = 5
 destination_found = False
 tree_connections = []
 obstacles = []
@@ -87,6 +98,8 @@ obstacle2 = Obstacle(100,150,30)
 obstacle3 = Obstacle(100,400,30)
 obstacle4 = Obstacle(300,500,30)
 
+
+
 # initialize the initial point 
 initialPoint = Point()
 initialPoint.coordinate = ()
@@ -99,6 +112,7 @@ destinationPoint = Point()
 destinationPoint.coordinate = ()
 destinationPoint.color = (255,0,0)
 destinationPoint.radius = 7
+
 
 
 while running:
@@ -124,69 +138,66 @@ while running:
 					pygame.draw.circle(screen,initialPoint.color,(destinationPoint.coordinate[0],destinationPoint.coordinate[1]),destinationPoint.radius,3)
 					count+=1
 
-
-	while not destination_found and count==2:
+	while not destination_found and count == 2:
+		pygame.display.flip()
+		# generate a random node - qRandom
+		qRandom = Point()
+		qRandomX = random.randint(0,600)
+		qRandomY = random.randint(0,600)
+		qRandom.coordinate = (qRandomX,qRandomY)
+		pygame.draw.circle(screen,(255,0,0),(qRandom.coordinate[0],qRandom.coordinate[1]),qRandom.radius,3)
 		
-		# generate a node at a random location in screen	
-		newPoint = Point()
-		newPointX = random.randint(0,600)
-		newPointY = random.randint(0,600)
-		newPoint.coordinate = (newPointX,newPointY)
-
-		# check if node is a valid node
-
-		# --> check if it is not a repeated node
-		for node in tree_connections:
-			if(node.coordinate == newPoint.coordinate):
-				continue
-		# --> check if the node is not within any obstacle
-		if(newPoint.isInObstacle()):
-			continue
-		
-		# find the node closest to the new node
+		# find node closest to qRandom from tree - qNear
 		neighbour_distances = []
 		for neighbour in tree_connections:
-			neighbour_distances.append(newPoint.calculateDistance(neighbour))
-
+			neighbour_distances.append(qRandom.calculateDistance(neighbour))
 		min_distance = 100000
 		min_distance_node_index = -1
-
 		for i in range(0,len(neighbour_distances)):
 			if(neighbour_distances[i]<min_distance):
 				min_distance = neighbour_distances[i]
 				min_distance_node_index = i
-
-		# --> check if obstacle between the closest node and the newPoint node
-		if(newPoint.isCrossingObstacle(tree_connections[min_distance_node_index])):
-			continue
+		qNear = tree_connections[min_distance_node_index]
 		
-		# --> add this node as a node to tree connections if the distance is less than then maximum connect length
-		if(min_distance < max_connect_length):
-			newPoint.parent = tree_connections[min_distance_node_index]
-			tree_connections.append(newPoint)
-			pygame.draw.circle(screen,newPoint.color,(newPoint.coordinate[0],newPoint.coordinate[1]),newPoint.radius,3)
-			pygame.draw.lines(screen,(100,100,100),False,[newPoint.coordinate,newPoint.parent.coordinate],1)
-		else:
+		# generate a node epsilon distance away from qNear in direction of qRandom - qNew
+		qNew = qNear.generateNewNode(qRandom,epsilon)
+		#qNew = qRandom
+		# check if qNew is a valid node
+		# --> check if not repeated
+		flag = False
+		for node in tree_connections:
+			if(node.coordinate == qNew.coordinate):
+				flag = True
+				break
+		if flag == True:
+			continue
+		# --> check if not within any obstacle
+		if(qNew.isInObstacle()):
 			continue
 
+		# check if obstacle between qNear and qNew
+		if(qNear.isCrossingObstacle(qNew,segments)):
+			continue
 
-		# check if we are close to the destination point
+		# add qNew to the tree
+		qNew.parent = qNear
+		tree_connections.append(qNew)
+		pygame.draw.circle(screen,qNew.color,(qNew.coordinate[0],qNew.coordinate[1]),qNew.radius,3)
+		pygame.draw.lines(screen,(100,100,100),False,[qNew.coordinate,qNew.parent.coordinate],1)
 
-		# --> check if the destination point is within reach
-		if(newPoint.calculateDistance(destinationPoint)<=max_connect_length):
-			destination_found=True
-			destinationPoint.parent = newPoint
-			# --> check if there is obstacle between the node and destination point
-			if(newPoint.isCrossingObstacle(newPoint.parent)):
+		# check if we are close to destination
+		# --> check if destination within reach
+		if(qNew.calculateDistance(destinationPoint)<=max_connect_length):
+			# --> check if obstacle between destination and qNew
+			if(qNew.isCrossingObstacle(destinationPoint,segments)):
 				continue
+			destinationPoint.parent = qNew
 			tree_connections.append(destinationPoint)
-			pygame.draw.lines(screen,(0,0,255),False,[destinationPoint.parent.coordinate,destinationPoint.coordinate])
-		
-
-		
+			pygame.draw.lines(screen,(100,100,100),False,[qNew.coordinate,destinationPoint.coordinate],1)
+			destination_found = True
 		pygame.display.flip()
+		
 
-	# construct the path to the start node
 	if(destination_found == True and show_stats == False) :
 		temp = copy.copy(destinationPoint)
 		while(temp.parent != None):
@@ -198,5 +209,9 @@ while running:
 		show_stats = True
 
 	pygame.display.flip()
+
+
+	
+		
 
 
